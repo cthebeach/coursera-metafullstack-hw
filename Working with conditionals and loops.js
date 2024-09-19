ca7b030da447963f1c0132028a7de94aef6330bc
cthebeach <christinebbeach@gmail.com>
2024-09-19 15:14:15 -0700

    Create Working with conditionals and loops.js

---
+++ Working with conditionals and loops.js
@@ -0,0 +1,23 @@
+/** Programmming with Javascript: Exercise Module 1 Working with conditionals and loops */
+
+
+
+for(var i=0; i<9; i++){
+    if (i<4) {
+        switch(i) {
+            case 1: 
+                i = "Gold medal";
+            break;
+            case 2: 
+                i = "Silver medal";
+            break;
+            case 3: 
+                i = "Bronze medal";
+            break;
+        
+        }
+        console.log (i);
+    } else {
+        console.log (i);
+    }
+}
