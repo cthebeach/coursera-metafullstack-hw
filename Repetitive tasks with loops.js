4c8e14f6a12aef08d6a03a8d4a543820d69b8f80
cthebeach <christinebbeach@gmail.com>
2024-10-08 14:56:19 -0700

    Add files via upload

---
+++ Repetitive tasks with loops.js
@@ -0,0 +1,36 @@
+/**Programmming with Javascript: Exercise Module 1 Repetitive tasks with loops */ 
+
+/** Task 1 */
+for(var i = 1; i <= 5; i++) {
+    console.log(i);
+}
+console.log('Counting complete!');
+
+/** Task 2 */
+for(var i = 5; i >=1; i--) {
+    console.log(i);
+}
+console.log('Counting finished!');
+
+/** Task 3 */
+var counter = 1;
+while(counter<=5) {
+    console.log(counter);
+    counter++;
+}
+console.log('Counting complete!');
+
+/** Task 4 */
+var counter = 5;
+while(counter>=1) {
+    console.log(counter);
+    counter = counter - 1;
+}
+console.log('Counting finished!');
+
+/** Task 5 */
+var date = 2018;
+while(date <= 2022) {
+    console.log(date);
+    date++;
+}
