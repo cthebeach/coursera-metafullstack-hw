04c393320aa6d7446b559b7e2f6727a6a1d4a549
cthebeach <christinebbeach@gmail.com>
2024-10-08 15:55:09 -0700

    completed assignment push

---
+++ Module2/CreatingArraysAndObjects.js
@@ -0,0 +1,20 @@
+// Module2 Exercise: Creating arrays and objects
+
+
+var clothes = [];
+
+clothes.push("hat");
+clothes.push("shirt");
+clothes.push("pants");
+clothes.push("dress");
+clothes.push("shoes");
+clothes.pop();
+clothes.push("jewelry");
+console.log(clothes[2]);
+
+var favCar = {};
+
+favCar.color = "blue";
+favCar.convertible = false;
+
+console.log(favCar);
